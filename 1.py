696df871f9f0fe68c587459557c4460a326f502e
Yevheniia <glaza.goluboyy@gmail.com>
2024-12-11 17:56:06 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+first = float(input("Введіть перше число: "))
+second = float(input("Введіть друге число: "))
+
+sum_result = first + second
+subtract_result = first - second
+product_result = first * second
+
+print("Результат:")
+print(f"Сума: {sum_result}")
+print(f"Різниця: {subtract_result}")
+print(f"Добуток: {product_result}")
